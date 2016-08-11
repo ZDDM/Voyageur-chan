fd739e318684921de3da3d6d8e99454660db6cfd
Zumo <apvictor00@hotmail.com>
2016-08-11 02:58:29 +0200

    Added early support for PM.

--- voyageur/sniffing.py
+++ voyageur/sniffing.py
@@ -69,7 +69,9 @@ class Capturer(object):
         finally:
             split = trailer.split("|")
 
-            if trailer.startswith("0"): # Normal messages.
+            if trailer.startswith("0") and len(split) > 2: # Normal messages.
+                if prefix == "You":
+                    self.location_filter = split[5]
                 if split[5] == self.location_filter:
                     if split[7] == "1" and split[8] != "None":
                         sfxmessage = "%s played an SFX: %s"%(prefix, split[8])
@@ -88,6 +90,12 @@ class Capturer(object):
                     self.queue.append(message)
                     if split[9] == "1" and split[8] != "None":
                         self.queue.append(sfxmessage)
+
+            elif trailer.startswith("0") and len(split) <= 2: # Private messages.
+                if prefix != "You":
+                    self.queue.append("(PM / %s -> You) %s" %(prefix, split[1]))
+                else:
+                    self.queue.append("(PM / You -> ????) %s" %(split[1]))
 
             elif trailer.startswith("1"): # OOC.
                 message = "(OOC) %s: %s"%(prefix, split[1])
